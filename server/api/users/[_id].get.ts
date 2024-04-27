80f77391607896bd08c17f56b940328b61e7bc70
shakiroye <oyeossiadewoye@gmail.com>
2024-04-28 00:05:21 +0200

    feat: users list and get user info

---
+++ server/api/users/[_id].get.ts
@@ -0,0 +1,7 @@
+export default defineEventHandler(async (event) => {
+  try {
+    return await UserSchema.findOne({ _id: event.context.params?._id });
+  } catch (error) {
+    return error;
+  }
+});
