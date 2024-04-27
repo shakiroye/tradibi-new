0cb7fdf79e5f45a7c43015d7467235433b798b1c
shakiroye <oyeossiadewoye@gmail.com>
2024-04-27 23:02:27 +0200

    chore: @types/jsonwebtoken installation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,5 +9,8 @@ export default defineNuxtConfig({
     modelsDir: "models",
   },
 
-  runtimeConfig: {},
+  runtimeConfig: {
+    jwtAccessSecret: process.env.JWT_ACCESS_TOKEN_SECRET,
+    jwtRefreshSecret: process.env.JWT_REFRESH_TOKEN_SECRET,
+  },
 });
