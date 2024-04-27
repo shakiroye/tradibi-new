5d8a5fc9d523ef7c9d596774fb8c702c1f820bf1
shakiroye <oyeossiadewoye@gmail.com>
2024-04-27 22:12:17 +0200

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["nuxt-mongoose"],
+
+  mongoose: {
+    uri: process.env.MONGODB_URI,
+    options: {},
+    modelsDir: "models",
+  },
+
+  runtimeConfig: {},
+});
